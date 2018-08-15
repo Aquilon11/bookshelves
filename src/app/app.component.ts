819aac653c60211f811c4df3fd6287fee6fcfc61
aquilon11 <aquilon11@gmail.com>
2018-08-15 14:44:42 +0200

    Authentification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import * as firebase from 'firebase'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'bookshelves';
+  constructor() {
+    var config = require('./serviceAccountKey.json')
+    firebase.initializeApp(config)
+  }
 }
